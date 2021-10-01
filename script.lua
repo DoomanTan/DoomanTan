c31a2842f3394d8b0bf60eb79b7c12c9d644accd
DoomanTan <91713165+DoomanTan@users.noreply.github.com>
2021-10-01 10:51:09 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Shoes Hub", "DarkTheme")
+
+local Tab = Window:NewTab("MENO UODATE")
+local Section = Tab:NewSection("Auto Fram")
